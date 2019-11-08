c360b919226d9c9f3a51635c6d8c1acd30f45b46
navkk <navkkrnair@gmail.com>
2019-11-08 16:58:38 +0530

    Changed

--- src/main/java/com/istio/PreferenceApplication.java
+++ src/main/java/com/istio/PreferenceApplication.java
@@ -1,7 +1,13 @@
 package com.istio;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,10 +18,33 @@ import io.opentracing.Tracer;
 @SpringBootApplication
 public class PreferenceApplication
 {
+	private static final Logger logger = LoggerFactory.getLogger(PreferenceApplication.class);
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(PreferenceApplication.class, args);
+		SpringApplication app = new SpringApplication(PreferenceApplication.class);
+		app.addListeners((e) ->
+		{
+			if (e instanceof ApplicationReadyEvent)
+			{
+				File file = new File(File.separatorChar + "var" + File.separatorChar + "ready");
+				logger.info("Creating file at: " + file);
+				if (!(file.exists()))
+				{
+					try
+					{
+						file.createNewFile();
+						logger.info("created file: " + file);
+					}
+					catch (IOException e1)
+					{
+						logger.info("Creating file failed");
+					}
+
+				}
+			}
+		});
+		app.run(args);
 	}
 
 	@Bean
