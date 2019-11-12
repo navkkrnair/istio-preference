f9c00ba6a56bfc7f7b2310b6b14c3fee5b1c42e1
navkkrnair <navkkrnair@gmail.com>
2019-11-12 13:44:16 +0530

    Changed code a little

--- src/main/java/com/istio/PreferenceController.java
+++ src/main/java/com/istio/PreferenceController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -20,7 +21,7 @@ public class PreferenceController
 
 	private final RestTemplate restTemplate;
 
-	@Value("${recommendations.api.url:http://recommendations:8080}")
+	@Value("${recommendations.api.url:http://recommendation:8080}")
 	private String remoteURL;
 
 	public PreferenceController(RestTemplate restTemplate)
@@ -32,38 +33,27 @@ public class PreferenceController
 	public ResponseEntity<?> getPreferences()
 	{
 		logger.info(">> getPreferences() called");
-		try
-		{
-			logger.info(">> Calling {}", remoteURL);
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
-			String                 response       = responseEntity.getBody();
-			return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
-		}
-		catch (HttpStatusCodeException ex)
-		{
-			logger.warn("Exception trying to get the response from recommendation service.", ex);
-			return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-				.body(
-						String.format(
-								RESPONSE_STRING_FORMAT,
-									String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
-		}
-		catch (RestClientException ex)
-		{
-			logger.warn("Exception trying to get the response from recommendation service.", ex);
-			return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-				.body(String.format(RESPONSE_STRING_FORMAT, ex.getMessage()));
-		}
+
+		logger.info(">> Calling {}", remoteURL);
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
+		String                 response       = responseEntity.getBody();
+		return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<String> handleException(HttpStatusCodeException ex)
+	{
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+			.body(
+					String.format(
+							RESPONSE_STRING_FORMAT,
+								String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
 	}
 
 	private String createHttpErrorResponseString(HttpStatusCodeException ex)
 	{
-		String responseBody = ex.getResponseBodyAsString().trim();
-		if (responseBody.startsWith("null"))
-		{
-			return ex.getStatusCode().getReasonPhrase();
-		}
-		return responseBody;
+		return ex.getResponseBodyAsString().trim();
+
 	}
 
 }
